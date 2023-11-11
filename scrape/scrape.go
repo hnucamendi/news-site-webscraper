90b31433a3eb7432816314e9dec6ef80817d11eb
Harold Arriola Nucamendi <55587068+hnucamendi@users.noreply.github.com>
2023-11-11 18:38:57 -0500

    working headlines scrape

--- scrape/scrape.go
+++ scrape/scrape.go
@@ -41,29 +41,17 @@ func NewScrape() *Scrape {
 
 func (s *Scrape) ScrapeTopHeadLines(c *colly.Collector) error {
 	c.OnHTML(".zone__items", func(e *colly.HTMLElement) {
-		// e.ForEach(".container.container_lead-package.cnn", func(_ int, c *colly.HTMLElement) {
-		e.ForEach(".container__title_url-text", func(_ int, t *colly.HTMLElement) {
-			fmt.Printf("Title: %s\n", t.Text)
-			e.ForEach(".container__headline-text", func(_ int, d *colly.HTMLElement) {
-				fmt.Printf("Description: %s\n", d.Text)
-				e.ForEach(".container__field-links", func(_ int, du *colly.HTMLElement) {
-					// fmt.Printf("DESC: %s\n", d.Text)
-					// fmt.Printf("Title: %s\nDescription: %s\nURL: %s\n", t.Text, d.Text, du.ChildAttr("a[href]", "href"))
-
-					// s.NewsSite.TopHeadlines = append(s.NewsSite.TopHeadlines, &TopHeadlines{
-					// 	NewsArticle: []*NewsArticle{
-					// 		{
-					// 			Title:       t.Text,
-					// 			Description: d.Text,
-					// 			AritcleURL:  fmt.Sprintf("https://us.cnn.com%s", du.ChildAttr("a[href]", "href")),
-					// 		},
-					// 	},
-					// })
-				})
-			})
+		e.ChildText(".container__field-links")
+		s.NewsSite.TopHeadlines = append(s.NewsSite.TopHeadlines, &TopHeadlines{
+			NewsArticle: []*NewsArticle{
+				{
+					Title:       e.ChildText(".container__title_url-text"),
+					Description: e.ChildText(".container__headline-text"),
+					AritcleURL:  fmt.Sprintf("https://us.cnn.com%s", e.ChildAttr("a[href]", "href")),
+				},
+			},
 		})
 	})
-	// })
 
 	c.OnRequest(func(r *colly.Request) {
 		s.NewsSite.Site = strings.Split(r.URL.Host, ".")[1]
@@ -80,14 +68,6 @@ func (s *Scrape) ScrapeTopHeadLines(c *colly.Collector) error {
 
 	fmt.Println(string(bytes))
 
-	// for _, v := range s.URLS {
-	// 	s.wg.Add(1)
-	// 	go func(v string) {
-	// 		defer s.wg.Done()
-	// 		c.Visit(v)
-	// 		fmt.Println(v)
-	// 	}(v)
-	// }
-
 	return nil
 }
+''
